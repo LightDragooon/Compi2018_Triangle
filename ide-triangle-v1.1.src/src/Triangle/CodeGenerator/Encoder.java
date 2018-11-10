58488059fc2f7948ee905fe0b352eab08dde6375
Josué Suárez <josuarez2297@gmail.com>
2018-11-10 16:19:03 -0600

    Generador de código de Repeats
    ** Falta for

--- ide-triangle-v1.1.src/src/Triangle/CodeGenerator/Encoder.java
+++ ide-triangle-v1.1.src/src/Triangle/CodeGenerator/Encoder.java
@@ -169,12 +169,7 @@ public final class Encoder implements Visitor {
   }
   
     public Object visitLocalDeclaration(LocalDeclaration ast, Object o) {
-        Frame frame = (Frame) o;
-        int extraSize1, extraSize2;
-        extraSize1 = ((Integer) ast.D1.visit(this, frame)).intValue();
-        Frame frame1 = new Frame (frame, extraSize1);
-        extraSize2 = ((Integer) ast.D2.visit(this, frame1)).intValue();
-        return new Integer(extraSize1 + extraSize2);
+       return null 
   }
 
     
